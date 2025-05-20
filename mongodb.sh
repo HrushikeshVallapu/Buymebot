b441d55ba4b7e59eeff283a021b12fc3d479b447
HrushikeshVallapu <hrushikeshvallapu>
2025-05-20 20:52:24 +0530

    day 4 autmtn of project using shellscript

--- mongodb.sh
+++ mongodb.sh
@@ -34,15 +34,15 @@ validate(){
 cp mongodb.repo /etc/yum.repos.d/mongodb.repo #copying mongodb.repo to the wanted location in vm
 validate $? "copying mongodb repo"
 
-dnf install mongodb-org -y 
+dnf install mongodb-org -y &>>$log_file
 validate $? "installing mongodb server"
 
-systemctl enable mongod 
-systemctl start mongod 
+systemctl enable mongod &>>$log_file
+systemctl start mongod &>>$log_file
 validate $? "enabling and  starting mongodb"
 
 sed -i 's/127.0.0.0/0.0.0.0/g' /etc/mongod.conf
 validate $? "update listen adress" 
 
-systemctl restart mongod
-validate $? "restarting mongodb" 
+systemctl restart mongod &>>$log_file
+validate $? "restarting mongodb " 
