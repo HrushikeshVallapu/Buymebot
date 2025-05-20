310572f9b9eb1117c748aabe0676c27e49d8088b
HrushikeshVallapu <hrushikeshvallapu>
2025-05-20 18:54:48 +0530

    day 4 autmtn of project using shellscript

--- buymebot.sh
+++ buymebot.sh
@@ -9,7 +9,7 @@ domain_name="buymebot.shop"
 for instance in ${instances[@]}
 do  
     instance_id=aws ec2 run-instances --image-id $ami_id --instance-type t3.micro --security-group-ids $sg_id --associate-public-ip-address --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId" --output json
-    if [ $instance_id != "frontend" ]
+    if [ $instance_id -ne "frontend" ]
     then 
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
     else
