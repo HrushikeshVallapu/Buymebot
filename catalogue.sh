ddcd0f23782e7c89a0fd52dc7c6839e873fdeb3f
HrushikeshVallapu <hrushikeshvallapu>
2025-05-20 21:21:18 +0530

    day 4 autmtn of project using shellscript

---
+++ catalogue.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+userid=$(id -u)
+r="\e[31m"
+g="\e[32m"
+y="\e[33m"
+n="\e[0m"
+logs_folder="var/log/buymebot-logs"
+script_name=$(echo $0 | cut -d "." -f1)
+log_file="$logs_folder/$script_name.log"
+script_dir=$PWD
+
+mkdir -p $logs_folder
+echo "script started executing at $(date)" | tee -a $log_file
+
+if [ $userid -ne 0 ]
+then 
+    echo -e "$r you are not root user $n" | tee -a $log_file
+    exit 1
+else
+    echo "preparing to start the installation" | tee -a $log_file
+fi
+
+validate(){
+    if [ $1 -eq 0 ]
+    then 
+        echo -e "$g installation of $2 success $n" | tee -a $log_file
+    else   
+        echo -e "$r installation of $2 failed $n" | tee -a $log_file
+        exit 1
+    fi
+}
+
+dnf module disable nodejs -y &>>$log_file
+validate $? "disabling nodejs"
+
+dnf module enable nodejs:20 -y &>>$log_file
+validate $? "enabling nodejs:20"
+
+dnf install nodejs -y &>>$log_file
+validate $? "installing nodejs:20"
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+validate $? "creating systemuser"
+
+mkdir -p /app &>>$log_file
+validate $? "making home dirctry for user"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$log_file
+validate $? "downloading catalogue zip file"
+
+cd /app &>>$log_file
+unzip /tmp/catalogue.zip &>>$log_file
+validate $? "unzipping catalogue"
+
+npm install &>>$log_file
+validate $? "installing dependencies"
+
+cp $script_dir/catalogue.service /etc/systemd/system/catalogue.service
+validate $? "copying catalogue service"
+
+systemctl daemon-reload &>>$log_file
+systemctl enable catalogue &>>$log_file
+systemctl start catalogue
+validate $? "starting catalogue"
+
+cp $script_dir/mongodb.repo /etc/yum.repos.d/mongo.repo
+dnf install mongodb-mongosh -y &>>$log_file
+validate $? "installing mongoDB client "
+
+mongosh --host mongodb.buymebot.sh </app/db/master-data.js &>>$log_file
+validate $? "loading master data into mongo"
