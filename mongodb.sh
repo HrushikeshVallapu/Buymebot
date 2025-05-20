475ec8678b4e0b4b01532e1b9c5cf3fdcbba263c
HrushikeshVallapu <hrushikeshvallapu>
2025-05-20 20:49:14 +0530

    day 4 autmtn of project using shellscript

---
+++ mongodb.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+userid=$(id -u)
+r="\e[31m"
+g="\e[32m"
+y="\e[33m"
+n="\e[0m"
+logs_folder="var/log/buymebot-logs"
+script_name=$(echo $0 | cut -d "." -f1)
+log_file="$logs_folder/$script_name.log"
+packages=("mysql" "nginx" "python3" "httpd")
+
+mkdir -p $logs_folder
+echo "script started executing at $(date)" | tee -a $log_file
+
+if [ $userid -ne 0 ]
+then 
+    echo -e "$r you are not root user $n" | tee -a $log_file
+    exit 1
+else
+    echo "preparing to start the installation" | tee -a $log_file
+fi
+
+validate(){
+    if [ $1 -eq 0 ]
+    then 
+        echo -e "$g installation of $2 success $n" | tee -a $log_file
+    else   
+        echo -e "$r installation of $2 failed $n" | tee -a $log_file
+        exit 1
+    fi
+}
+
+cp mongodb.repo /etc/yum.repos.d/mongodb.repo #copying mongodb.repo to the wanted location in vm
+validate $? "copying mongodb repo"
+
+dnf install mongodb-org -y 
+validate $? "installing mongodb server"
+
+systemctl enable mongod 
+systemctl start mongod 
+validate $? "enabling and  starting mongodb"
+
+sed -i 's/127.0.0.0/0.0.0.0/g' /etc/mongod.conf
+validate $? "update listen adress" 
+
+systemctl restart mongod
+validate $? "restarting mongodb" 
