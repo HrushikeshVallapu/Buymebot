ae719571d77ddd33f0ecae1d5a04101cc1a2280f
HrushikeshVallapu <hrushikeshvallapu>
2025-05-20 20:21:56 +0530

    day 4 autmtn of project using shellscript

--- buymebot.sh
+++ buymebot.sh
@@ -6,16 +6,14 @@ instances=("mongodb" "catalogue" "frontend")
 zone_id="Z06633071XX7HF3WWN7FZ"
 domain_name="buymebot.shop"
 
-for instance in ${instances[@]}
-do  
-    instance_id=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0142341bd063dfed3  --associate-public-ip-address --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
-    aws ec2 wait instance-running --instance-ids "$instance_id"
-    if [ $instance_id != "frontend" ]
-    then 
-        IP=$(aws ec2 describe-instances --instance-ids $instance_id --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+for instance in ${INSTANCES[@]}
+do
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0142341bd063dfed3 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    if [ $instance != "frontend" ]
+    then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
     else
-        IP=$(aws ec2 describe-instances --instance-ids $instance_id --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
     fi
     echo "$instance IP address: $IP"
-
 done
