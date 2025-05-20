ae6c9a5e1de85dd51838c829c7e45b126da4d964
HrushikeshVallapu <hrushikeshvallapu>
2025-05-20 18:46:58 +0530

    day 4 autmtn of project using shellscript

---
+++ buymebot.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ami_id="ami-09c813fb71547fc4f"
+sg_id="sg-0142341bd063dfed3"
+instances=("mongodb" "catalogue" "frontend")
+zone_id="Z06633071XX7HF3WWN7FZ"
+domain_name="buymebot.shop"
+
+for instance in ${instances[@]}
+do  
+    instance_id=aws ec2 run-instances --image-id $ami_id --instance-type t3.micro --security-group-ids $sg_id --associate-public-ip-address --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId" --output json
+    if [ $instance_id != "frontend"]
+    then 
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+    fi
+    echo "$instance IP address: $IP"
+
+done
